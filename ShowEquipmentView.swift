e5511e7b4ff8d4d2558b6ead567a4fe5fbb897a2
2nd-nakayoshi <nakayoshi@2ndwave.jp>
2021-05-14 15:38:56 +0900

    navigationViewを使用

--- ShowEquipmentView.swift
+++ ShowEquipmentView.swift
@@ -12,23 +12,20 @@ struct ShowEquipmentView: View {
     @State private var showingModal = false
     let equipments = ["機材1", "機材2", "機材3", "機材4", "機材5", "機材6", "機材7", "機材8", "機材9", "機材10", "機材11", "機材12", "機材13"]
     var body: some View {
-
-//        NavigationView {
-        HStack(spacing: 50){
-//            Text("新規作成")
-            Spacer()
-            Button("+", action: {
+        NavigationView {
+            List {
+                ForEach(0 ..< equipments.count) { index in
+                    Text(equipments[index])
+                }
+            }
+            .navigationBarItems(trailing: Button(action: {
                 self.showingModal.toggle()
-            }).padding().sheet(isPresented: $showingModal) {
+            }, label: {
+                Text("+")
+            }).sheet(isPresented: $showingModal) {
                 EquipmentModalView()
-            }
+            })
         }
-        List {
-            ForEach(0 ..< equipments.count) { index in
-                Text(equipments[index])
-            }
-        }
-
     }
 }
 
