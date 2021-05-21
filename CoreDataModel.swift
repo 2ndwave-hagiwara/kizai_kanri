79a101548a8fb6833a2f504a93f14471dabb1014
2nd-nakayoshi <nakayoshi@2ndwave.jp>
2021-05-21 15:46:01 +0900

    機材画面遷移　一旦コミット

---
+++ CoreDataModel.swift
@@ -0,0 +1,15 @@
+//
+//  CoreDataModel.swift
+//  kizai_kanri
+//
+//  Created by 仲吉一馬 on 2021/05/21.
+//  Copyright © 2021 hagiwara. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+import CoreData
+
+class CoreDataModel{
+}
